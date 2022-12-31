a9d0d8f1368ad67f6b718fc7e5abb0674e910565
temidayo <temmideee@gmail.com>
2022-12-31 02:32:52 +0100

    added babel

---
+++ dist/index.js
@@ -0,0 +1,54 @@
+"use strict";
+
+var _express = _interopRequireDefault(require("express"));
+var _mongoose = _interopRequireDefault(require("mongoose"));
+var _cors = _interopRequireDefault(require("cors"));
+var _dotenv = _interopRequireDefault(require("dotenv"));
+var _cookieParser = _interopRequireDefault(require("cookie-parser"));
+var _userRoutes = _interopRequireDefault(require("../routes/user-routes"));
+var _postRoutes = _interopRequireDefault(require("../routes/post-routes"));
+var _bookmarkRoutes = _interopRequireDefault(
+  require("../routes/bookmark-routes")
+);
+var _messageRoutes = _interopRequireDefault(
+  require("../routes/message-routes")
+);
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : { default: obj };
+}
+_dotenv.default.config();
+const app = (0, _express.default)();
+app.use(
+  (0, _cors.default)({
+    origin: [
+      "https://insttagg.herokuapp.com",
+      "https://api.cloudinary.com",
+      "http://localhost:3000",
+      "https://insttagg-client.vercel.app",
+    ],
+    credentials: true,
+    allowedHeaders:
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+  })
+);
+app.use(_express.default.json());
+app.use((0, _cookieParser.default)());
+app.set("trust proxy", 1);
+app.use("/api", _userRoutes.default);
+app.use("/api/posts", _postRoutes.default);
+app.use("/api/bookmarks", _bookmarkRoutes.default);
+app.use("/api/messages", _messageRoutes.default);
+_mongoose.default.set("strictQuery", false);
+_mongoose.default
+  .connect(
+    `mongodb+srv://temidayo:H26VmfkBFuvc5Het@undev.nc7mk.mongodb.net/undev?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    app.listen(process.env.PORT);
+  })
+  .then(() => {
+    console.log("Connected To Database....");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
